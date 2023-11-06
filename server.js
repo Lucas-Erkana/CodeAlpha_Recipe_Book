7ba4a909ea666506416b70f8fa01241eb1d11c63
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-06 13:20:40 +0200

    add search author functionality

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useUrlNewParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useUrlNewParser: true} )
 const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose'))
