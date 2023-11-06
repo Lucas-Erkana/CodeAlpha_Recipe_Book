43c5fb3ac123a92f131153640180b0e1cd246aec
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-06 12:14:22 +0200

    Add author and error message

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load()
 } 
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-
 mongoose.connect(process.env.DATABASE_URL, { useUrlNewParser: true})
 const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 app.listen(process.env.PORT || 3000)
