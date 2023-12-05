ce7594c96a8942c855e7bf7b20e37171db0634db
Lucas David Erkana <lderkana21x@gmail.com>
2023-12-05 19:36:27 +0200

    Implemented AddEditRecipe component with addRecipe, updateRecipe, and fetchRecipe API calls
    - Imported React, useState, useEffect, useParams, useHistory, useSelector, and useDispatch
    - Imported addRecipe, updateRecipe, and fetchRecipe functions from '../../services/api'
    - Imported setRecipes action creator from '../../redux/slices/recipeSlice'
    - Defined AddEditRecipe component as a functional component
    - Extracted the 'id' parameter from the URL using useParams hook
    - Initialized history with useHistory hook for programmatic navigation
    - Initialized dispatch with useDispatch hook
    - Initialized recipes state variable using useSelector hook to retrieve the recipes from the Redux store
    - Initialized formData state variable using useState hook with an initial value of an object containing title and description properties
    - Implemented useEffect hook to fetch the recipe data from the API and update the formData state when the 'id' parameter changes
    - Implemented handleInputChange function to update the formData state when the user inputs new data
    - Implemented handleSubmit function for handling form submission
      - Calls the addRecipe or updateRecipe API function with the formData
      - Updates the Redux store to add or update the recipe data
      - Handles submission errors by logging the error to the console
      - Navigates to the home page after successful submission
    - Rendered a form for adding or editing a recipe with input fields for title and description, and a submit button that displays "Add Recipe" or "Update Recipe" based on whether an 'id' parameter is present in the URL

--- src/components/recipes/AddEditRecipe.js
+++ src/components/recipes/AddEditRecipe.js
@@ -1,5 +1,102 @@
-function AddEditRecipe() {
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { addRecipe, updateRecipe, fetchRecipe } from '../../services/api';
+import { setRecipes } from '../../redux/slices/recipeSlice';
 
-  }
-  
-  export default AddEditRecipe;
+function AddEditRecipe() {
+  const { id } = useParams();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const recipes = useSelector((state) => state.recipes);
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+  });
+
+  useEffect(() => {
+    if (id) {
+      async function fetchRecipeData() {
+        try {
+          const response = await fetchRecipe(id);
+          setFormData({
+            title: response.data.recipe.title,
+            description: response.data.recipe.description,
+          });
+        } catch (error) {
+          // Handle error
+          console.error(error);
+        }
+      }
+      fetchRecipeData();
+    }
+  }, [id]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (id) {
+        // Update existing recipe
+        await updateRecipe(id, formData);
+        const updatedRecipes = recipes.map((recipe) =>
+          recipe.id === id ? { ...recipe, ...formData } : recipe
+        );
+        dispatch(setRecipes(updatedRecipes));
+      } else {
+        // Add new recipe
+        const response = await addRecipe(formData);
+        dispatch(setRecipes([...recipes, response.data.recipe]));
+      }
+      history.push('/');
+    } catch (error) {
+      // Handle error
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>{id ? 'Edit Recipe' : 'Add Recipe'}</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <textarea
+            className="form-control"
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          {id ? 'Update Recipe' : 'Add Recipe'}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddEditRecipe;
